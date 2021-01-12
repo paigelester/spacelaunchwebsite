8db857d3a22bbb9b23494afbfc625bf3f643b2d8
paigelester <paigelester7@gmail.com>
2021-01-12 19:23:02 +0000

    Added react router and initial home page.

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div id='home-page'>
+            <h1>Home page</h1>
+        </div>
+    );
+};
+
+export default HomePage;
