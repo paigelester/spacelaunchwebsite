4aaf5caf56f8737672164b0e0f3354603c32895d
paigelester <paigelester7@gmail.com>
2021-04-16 22:08:34 +0100

    Added moment js for sort dates for countdown and updated bootstrap versions.

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,17 +1,88 @@
 import React from 'react';
 import { useResource } from 'rest-hooks';
 
-import { UpcomingLaunchResource } from 'api/LaunchResource';
+import { UpcomingLaunchResource, PreviousLaunchResource } from 'api/LaunchResource';
 
 import NextLaunch from './NextLaunch';
 
+
+import { Container, Row, Col, Image, Card, Badge } from 'react-bootstrap';
+const getBadge = (launchStatus: string | undefined): JSX.Element => {
+    let backgroundColour: string = "light";
+
+    if (launchStatus !== undefined) {
+        switch (launchStatus) {
+            case "Go":
+            case "Go for Launch":
+            case "Success":
+            case "Launch Successful":
+                backgroundColour = "success";
+                break;
+            case "TBD":
+                backgroundColour = "warning";
+                break;
+        }
+    }
+
+    return <Badge bg={backgroundColour}>{launchStatus}</Badge>;
+};
+
 const HomePage = () => {
 
-    const upcomingLaunch = useResource(UpcomingLaunchResource.list(), { limit: 1 });
+    const { results, ...launchesListDetails } = useResource(UpcomingLaunchResource.list(), { limit: 2 });
+    // const previousLaunches = useResource(PreviousLaunchResource.list(), { limit: 1 });
+
+    // const nextLaunch: UpcomingLaunchResource = results[0];
+    // const upcomingLaunch: UpcomingLaunchResource = results[1];
 
     return (
         <div id='home-page'>
-            {upcomingLaunch.results.length === 1 && <NextLaunch {...upcomingLaunch.results[0]} />}
+            {results && (
+                <NextLaunch
+                    name={results[0].name}
+                    image={results[0].image}
+                    status={results[0].status!.name}
+                    launchWindowStart={results[0].window_start}
+                    missionDescription={results[0].mission!.description}
+                    launchProviderName={results[0].launch_service_provider!.name}
+                    launchProviderType={results[0].launch_service_provider!.type} />
+            )}
+            <div className="container-fluid m-0 p-0">
+                <div className="row">
+                    <div className="col-sm-6 p-0">
+                        <div className="card">
+                            <div className="card-header">
+                                <h4>Previous launch</h4>
+                                <div className="float-end">{getBadge("Success")}</div>
+                            </div>
+                            <div className="card-body">
+                                {/* <h5>{previousLaunches.results[0].name}</h5> */}
+                                {/* <p>{previousLaunches.results[0].mission!.description}</p> */}
+                                <button>Read more</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-6 p-0">
+                        <div className="card">
+                            <div className="card-header">
+                                <h4>Upcoming launch</h4>
+                                <div className="float-end">{getBadge(results[1].status?.name)}</div>
+                            </div>
+                            <div className="card-body">
+                                <h5>{results[1].name}</h5>
+                                <p>{results[0].mission!.description}</p>
+                                <button>Read more</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                News (Upcoming events, expedictions, landings, launches)
+            </div>
+            <div>
+                Agencies
+            </div>
         </div>
     );
 };
--- src/pages/Home/NextLaunch.tsx
+++ src/pages/Home/NextLaunch.tsx
@@ -1,24 +1,70 @@
 import React from 'react';
 
-import './NextLaunch.scss'
+import { Container, Row, Col, Image, Card, Badge } from 'react-bootstrap';
 
-import { UpcomingLaunchResource } from 'api/LaunchResource';
+import Countdown from 'components/features/Countdown';
 
-const NextLaunch = (props: Partial<UpcomingLaunchResource>) => {
+const getBadge = (launchStatus: string | undefined): JSX.Element => {
+    let backgroundColour: string = "light";
+
+    if (launchStatus !== undefined) {
+        switch (launchStatus) {
+            case "Go for Launch":
+            case "Go":
+            case "Success":
+            case "Launch Successful":
+                backgroundColour = "success";
+                break;
+            case "TBD":
+            case "To Be Determined":
+                backgroundColour = "warning";
+                break;
+        }
+    }
+
+    return <Badge bg={backgroundColour}>{launchStatus}</Badge>;
+};
+
+interface NextLaunchProps {
+    name: string;
+    image: string;
+    status: string;
+    missionDescription: string;
+    launchWindowStart: string;
+    launchProviderName: string;
+    launchProviderType: string;
+}
+
+const NextLaunch = (props: NextLaunchProps) => {
     return (
-        <div className='next-launch'>
-            <div className='next-launch-image-container'>
-                <img src={props.image} alt='launch' />
-            </div>
-
-            <div className='next-launch-details shadow p-3 mb-5 bg-body rounded'>
-                <div className='float-right'>
-                    <span className="badge badge-secondary">{props.status?.abbrev}</span>
-                </div>
-
-                <h1 className='h6 mb-4'>{props.name}</h1>
-                <p>{props.status?.description}</p>
-            </div>
+        <div className='shadow-lg'>
+            <Container fluid>
+                <Row>
+                    <Col md={12} lg={9} className="g-0">
+                        <div className="position-relative overflow-hidden">
+                            <Image src={props.image} alt='launch' className="img-fluid" />
+                            <div className="position-absolute top-0 w-100 h-100 o-75 bg-dark"></div>
+                            <div className="position-absolute top-50 start-50 end-0 translate-middle text-center w-100">
+                                <p className="text-uppercase text-center mb-1 fs-2">Time to launch</p>
+                                <Countdown targetDate={props.launchWindowStart} />
+                            </div>
+                        </div>
+                    </Col>
+                    <Col className="g-0">
+                        <Card className="h-100">
+                            <Card.Body>
+                                <div className='float-start'>
+                                    <p className="d-inline fs-6 text-muted border-end border-dark pe-2 me-2">{props.launchProviderName}</p>
+                                    <p className="d-inline fs-6 text-muted">{props.launchProviderType}</p>
+                                </div>
+                                <div className='float-end'>{getBadge(props.status)}</div>
+                                <h4 className="pt-5 pb-2">{props.name}</h4>
+                                <p>{props.missionDescription}</p>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
